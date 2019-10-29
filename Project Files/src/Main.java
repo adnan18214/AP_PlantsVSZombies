1bcaa662621a15ab24e7cf27c9846a870f61b024
Adwit <adwit18276@iiitd.ac.in>
2019-10-30 02:20:58 +0530

    First try Scenebuilder
    Welcome Logo page and Select Player menu designed.

---
+++ Project Files/src/Main.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("./FXMLs/welcomeLogo.fxml"));
+        primaryStage.setTitle("Plants VS Zombies - AP Project");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+}
