4c12a2354e2e0286f2a6e694a107e6880c539871
Adwit <adwit18276@iiitd.ac.in>
2019-11-02 02:19:02 +0530

    All buttons connected with transitions
    Except Lawn and Load Game

---
+++ Project Files/src/FXMLs/HighScoreController.java
@@ -0,0 +1,16 @@
+package FXMLs;
+
+import gameRunner.Main;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+public class HighScoreController {
+    @FXML
+    private void goBackToMainMenu(ActionEvent actionEvent) {
+        Node source = (Node) actionEvent.getSource();
+        Stage primaryStage = (Stage) source.getScene().getWindow();
+        primaryStage.setScene(Main.getLastScene());
+    }
+}
--- Project Files/src/FXMLs/mainMenuController.java
+++ Project Files/src/FXMLs/mainMenuController.java
@@ -2,19 +2,15 @@ package FXMLs;
 
 import gameRunner.Main;
 import javafx.animation.ScaleTransition;
-import javafx.application.Platform;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.effect.Glow;
 import javafx.scene.effect.InnerShadow;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -22,15 +18,21 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class LevelController implements Initializable{
-    @FXML
-    private ImageView level1;
-    @FXML
-    private ImageView level2;
-    @FXML
-    private ImageView level3;
+public class mainMenuController implements Initializable {
     @FXML
     private ImageView shade;
+    @FXML
+    private ImageView loadGame;
+    @FXML
+    private ImageView chooseLevel;
+    @FXML
+    private ImageView settings;
+    @FXML
+    private ImageView highScores;
+    @FXML
+    private ImageView startGame;
+    @FXML
+    private ImageView logOut;
 
     @FXML
     private void makeGlow(MouseEvent mouseEvent) {
@@ -62,7 +64,37 @@ public class LevelController implements Initializable{
     }
 
     @FXML
-    private void goBackToMainMenu(ActionEvent actionEvent) {
+    private void logOut(MouseEvent mouseEvent) {
+        ImageView button = (ImageView) mouseEvent.getSource();
+        button.setEffect(null);
+
+        try {
+            Parent next = FXMLLoader.load(getClass().getClassLoader().getResource("./FXMLs/confirmLogOut.fxml"));
+            Stage primaryStage = (Stage) ((ImageView) mouseEvent.getSource()).getScene().getWindow();
+            Main.saveScene(primaryStage.getScene());
+            primaryStage.setScene(new Scene(next));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void showHighscores(MouseEvent mouseEvent) {
+        ImageView button = (ImageView) mouseEvent.getSource();
+        button.setEffect(null);
+
+        try {
+            Parent next = FXMLLoader.load(getClass().getClassLoader().getResource("./FXMLs/highScore.fxml"));
+            Stage primaryStage = (Stage) ((ImageView) mouseEvent.getSource()).getScene().getWindow();
+            Main.saveScene(primaryStage.getScene());
+            primaryStage.setScene(new Scene(next));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void chooseLevel(MouseEvent mouseEvent) {
         ScaleTransition close  = new ScaleTransition(Duration.seconds(1), shade);
         close.setByX(-78);
         close.setByY(-73);
@@ -71,7 +103,7 @@ public class LevelController implements Initializable{
 
         close.setOnFinished((e)-> {
             try {
-                Parent next = FXMLLoader.load(getClass().getClassLoader().getResource("./FXMLs/mainMenu.fxml"));
+                Parent next = FXMLLoader.load(getClass().getClassLoader().getResource("./FXMLs/Level.fxml"));
                 Stage primaryStage = (Stage) shade.getScene().getWindow();
                 primaryStage.setScene(new Scene(next));
             } catch (IOException ex) {
