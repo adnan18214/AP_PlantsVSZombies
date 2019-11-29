99991488a6150103fcbb900f6ff7bbffd307f9ef
Adnan Ali <adnan18214@iiitd.ac.in>
2019-11-30 01:53:28 +0530

    Zombieland made

---
+++ Project Files/src/allClasses/Zombie.java
@@ -0,0 +1,21 @@
+package allClasses;
+
+import java.util.ArrayList;
+
+public class Zombie
+{
+    private int posX;
+    private int posY;
+    private int health;
+    private ArrayList<Integer> shuffle = new ArrayList<Integer>();
+
+    public Zombie()
+    {
+        this.posX = 1100;
+        this.health = 30;
+        //this.posY = random.randint;
+        shuffle.add(325);
+        shuffle.add(540);
+        shuffle.add(425);
+    }
+}
